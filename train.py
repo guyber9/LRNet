bddf76d054ce333b0b4f817e6462eb3da105e9b1
guyber9 <guyberger9@gmail.com>
2021-05-06 16:37:55 +0300

    first draft of LRNet + train.py

---
+++ train.py
@@ -0,0 +1,78 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+import utils as utils
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=True,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.test_batch_size}
+    if use_cuda:
+        cuda_kwargs = {'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True}
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+
+    print ('Reading Database..')
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+    dataset1 = datasets.MNIST('../data', train=True, download=True,
+                       transform=transform)
+    dataset2 = datasets.MNIST('../data', train=False,
+                       transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+
+    model = utils.LRNet().to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    # optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    print ("###################################")
+    print ("training..")
+    print ("num of epochs: " + str(args.epochs))
+    print ("###################################")
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, args.epochs + 1):
+        utils.train(args, model, device, train_loader, optimizer, epoch)
+        utils.test(model, device, test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,203 @@
+import math
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.nn.parameter import Parameter
+from torch.nn import functional as F
+from torch.nn import init
+import numpy as np
+from torch.nn.modules.conv import _single, _pair, _triple, _reverse_repeat_tuple
+import numpy as np
+from torch.nn.common_types import _size_1_t, _size_2_t, _size_3_t
+
+
+class LRNet(nn.Module):
+
+    def __init__(self):
+        super(LRNet, self).__init__()
+        self.conv1 = myConv2d(1, 32, 5, 1)
+        self.conv2 = myConv2d(32, 64, 5, 1)
+        self.dropout1 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)  # 32 x 24 x 24
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2) # 32 x 12 x 12
+        x = self.dropout1(x)
+        x = self.conv2(x) # 64 x 8 x 8
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2) # 64 x 4 x 4
+        x = self.dropout2(x)
+        x = torch.flatten(x, 1) # 1024
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        # output = F.softmax(x)
+        output = x
+        return output
+
+class myConv2d(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: _size_2_t,
+        stride: _size_2_t = 1,
+        padding: _size_2_t = 0,
+        dilation: _size_2_t = 1,
+        groups: int = 1,
+        clusters: int = 3,
+    ):
+        super().__init__()
+        self.in_channels, self.out_channels, self.kernel_size, self.stride, self.padding, self.dilation, self.groups, self.clusters = in_channels, out_channels, kernel_size, stride, padding, dilation, groups, clusters
+        transposed = True
+        if transposed:
+            D_0, D_1, D_2, D_3 = out_channels, in_channels, kernel_size, kernel_size
+            weight_theta = torch.Tensor(out_channels, in_channels, kernel_size, kernel_size, clusters)
+        else:
+            D_0, D_1, D_2, D_3 = in_channels, out_channels, kernel_size, kernel_size
+            weight_theta = torch.Tensor(in_channels, out_channels, kernel_size, kernel_size, clusters)
+        test_weight = torch.Tensor(D_0, D_1, D_2, D_3)
+        self.weight_theta = nn.Parameter(test_weight)
+        self.weight_theta = nn.Parameter(weight_theta)  # nn.Parameter is a Tensor that's a module parameter.
+        bias = torch.Tensor(out_channels)
+        self.bias = Parameter(bias)
+        self.discrete_val = torch.tensor([[-1.0, 0.0, 1.0]])
+        self.discrete_val.requires_grad = False
+        # self.discrete_square_val = torch.tensor([[1.0, 0.0, 1.0]], dtype=torch.float32)
+        self.discrete_square_val = self.discrete_val * self.discrete_val
+        self.discrete_square_val.requires_grad = False
+
+        if transposed:
+            discrete_mat = self.discrete_val.unsqueeze(1).repeat(out_channels, in_channels, kernel_size, kernel_size, 1)
+            discrete_square_mat = self.discrete_square_val.unsqueeze(1).repeat(out_channels, in_channels, kernel_size, kernel_size, 1)
+        else:
+            discrete_mat = self.discrete_val.unsqueeze(1).repeat(in_channels, out_channels, kernel_size,kernel_size, 1)
+            discrete_square_mat = self.discrete_square_val.unsqueeze(1).repeat(out_channels, in_channels, kernel_size, kernel_size, 1)
+        self.discrete_mat = nn.Parameter(discrete_mat)
+        self.discrete_square_mat = nn.Parameter(discrete_square_mat)
+
+        self.discrete_mat.requires_grad = False
+        self.discrete_square_mat.requires_grad = False
+
+        self.softmax = torch.nn.Softmax(dim=4)
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        # self.reset_train_parameters()
+        init.kaiming_uniform_(self.weight_theta, a=math.sqrt(5))
+        # init.uniform_(self.weight_theta, -1, 1)
+        # init.constant_(self.weight_theta, 1)
+        if self.bias is not None:
+            bound = 1 / math.sqrt(5)
+            init.uniform_(self.bias, -bound, bound)
+
+        print("Initializing Test Weights: \n")
+        my_array = [];
+        for i, val_0 in enumerate(self.weight_theta):
+            my_array_0 = [];
+            for j, val_1 in enumerate(val_0):
+                my_array_1 = [];
+                for m, val_2 in enumerate(val_1):
+                    my_array_2 = [];
+                    for n, val_3 in enumerate(val_2):
+                        softmax_func = torch.nn.Softmax()
+                        theta = softmax_func(val_3, dim=1)
+                        values = torch.multinomial(theta, 1) - 1
+                        # print("\ntheta: " + str(theta))
+                        # print("\nvalues: " + str(values))
+                        my_array_2.append(values)
+                    my_array_1.append(my_array_2)
+                my_array_0.append(my_array_1)
+            my_array.append(my_array_0)
+        test_weight = torch.tensor(my_array, dtype=torch.float32)
+        self.test_weight = nn.Parameter(test_weight)
+
+    def reset_train_parameters(self) -> None:
+        print("Initializing Train Weights: \n")
+        p_max = 0.95
+        p_min = 0.05
+        wl = 0.7143
+        p_w_0 = p_max - (p_max - p_min) * wl
+        p_w_1 = 0.5 * (1 + (wl / (1 - p_w_0)))
+        prob = np.array([(1 - p_w_1 - p_w_0), p_w_0, p_w_1])
+        my_array = []
+        for i, val_0 in enumerate(self.weight_theta):
+            my_array_0 = [];
+            for j, val_1 in enumerate(val_0):
+                my_array_1 = [];
+                for m, val_2 in enumerate(val_1):
+                    my_array_2 = [];
+                    for n, val_3 in enumerate(val_2):
+                        my_array_2.append(prob)
+                    my_array_1.append(my_array_2)
+                my_array_0.append(my_array_1)
+            my_array.append(my_array_0)
+        weight_theta = torch.tensor(my_array, dtype=torch.float32)
+        self.weight_theta = nn.Parameter(weight_theta)
+
+    def forward(self, input: Tensor) -> Tensor:
+        # E[X] calc
+        prob_mat = self.softmax(self.weight_theta)
+        # prob_mat_clamp = torch.clamp(prob_mat, 0.05, 0.95)
+        mean_tmp = prob_mat * self.discrete_mat
+        mean = torch.sum(mean_tmp, dim=4)
+
+        # E[x^2]
+        mean_square_tmp = prob_mat * self.discrete_square_mat
+        mean_square = torch.sum(mean_square_tmp, dim=4)
+
+        # E[x] ^ 2
+        mean_pow2 = mean * mean
+
+        # Var (E[x^2] - E[x]^2)
+        sigma_square = mean_square - mean_pow2
+
+        z0 = F.conv2d(input, mean, self.bias, self.stride, self.padding, self.dilation, self.groups)
+        z1 = F.conv2d((input * input), sigma_square, None, self.stride, self.padding, self.dilation, self.groups)
+        epsilon = torch.rand(z1.size())
+        m = z0
+        v = torch.sqrt(z1)
+        return m + epsilon * v
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        # loss = F.nll_loss(output, target)
+        loss = F.cross_entropy(output, target) # <- was better
+        torch.autograd.set_detect_anomaly(True)
+        loss.backward(retain_graph=True)
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
