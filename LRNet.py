5e801c256f3a7e278bd71ff77d99b8fa8d807274
guyber9 <guyberger9@gmail.com>
2021-06-07 08:45:24 +0300

    optimize for GPU

--- LRNet.py
+++ LRNet.py
@@ -481,14 +481,14 @@ def train(args, model, device, train_loader, optimizer, epoch):
             if args.full_prec:
                 loss = F.cross_entropy(output, target)
             else:
-                loss = F.cross_entropy(output, target)
-                # loss = F.cross_entropy(output, target) + probability_decay * (torch.norm(model.conv1.alpha, 2) + torch.norm(model.conv1.betta, 2)
-                #                                  + torch.norm(model.conv2.alpha, 2) + torch.norm(model.conv2.betta, 2)
-                #                                  + torch.norm(model.conv3.alpha, 2) + torch.norm(model.conv3.betta, 2)
-                #                                  + torch.norm(model.conv4.alpha, 2) + torch.norm(model.conv4.betta, 2)
-                #                                  + torch.norm(model.conv5.alpha, 2) + torch.norm(model.conv5.betta, 2)
-                #                                  + torch.norm(model.conv6.alpha, 2) + torch.norm(model.conv6.betta, 2)) \
-                #                                  + weight_decay * (torch.norm(model.fc1.weight, 2) + (torch.norm(model.fc2.weight, 2)))
+                # loss = F.cross_entropy(output, target)
+                loss = F.cross_entropy(output, target) + probability_decay * (torch.norm(model.conv1.alpha, 2) + torch.norm(model.conv1.betta, 2)
+                                                 + torch.norm(model.conv2.alpha, 2) + torch.norm(model.conv2.betta, 2)
+                                                 + torch.norm(model.conv3.alpha, 2) + torch.norm(model.conv3.betta, 2)
+                                                 + torch.norm(model.conv4.alpha, 2) + torch.norm(model.conv4.betta, 2)
+                                                 + torch.norm(model.conv5.alpha, 2) + torch.norm(model.conv5.betta, 2)
+                                                 + torch.norm(model.conv6.alpha, 2) + torch.norm(model.conv6.betta, 2)) \
+                                                 + weight_decay * (torch.norm(model.fc1.weight, 2) + (torch.norm(model.fc2.weight, 2)))
         else:
             if args.full_prec:
                 loss = F.cross_entropy(output, target)
