2b768ea8402e99bea8641accedf7cde0aa7a8b47
guyber9 <guyberger9@gmail.com>
2021-06-16 09:20:03 +0300

    optimize for GPU

--- LRNet.py
+++ LRNet.py
@@ -489,11 +489,12 @@ def train(args, model, device, train_loader, optimizer, epoch, f=None):
                                                  + torch.norm(model.conv4.alpha, 2) + torch.norm(model.conv4.betta, 2)
                                                  + torch.norm(model.conv5.alpha, 2) + torch.norm(model.conv5.betta, 2)
                                                  + torch.norm(model.conv6.alpha, 2) + torch.norm(model.conv6.betta, 2)) \
-                                                 + weight_decay * (torch.norm(model.fc1.weight, 2) + (torch.norm(model.fc2.weight, 2)))
+                       + weight_decay * (torch.norm(model.conv1.bias, 2) + torch.norm(model.conv2.bias, 2) \
+                                         + torch.norm(model.conv3.bias, 2) + torch.norm(model.conv4.bias, 2) \
+                                         + torch.norm(model.conv5.bias, 2) + torch.norm(model.conv6.bias, 2)) \
+                       + weight_decay * (torch.norm(model.fc1.weight, 2) + (torch.norm(model.fc2.weight, 2)))
 
-        # + weight_decay * (torch.norm(model.conv1.bias, 2) + torch.norm(model.conv2.bias, 2) \
-        #                   + torch.norm(model.conv3.bias, 2) + torch.norm(model.conv4.bias, 2) \
-        #                   + torch.norm(model.conv5.bias, 2) + torch.norm(model.conv6.bias, 2)) \
+
         else:
             if args.full_prec:
                 loss = F.cross_entropy(output, target)
