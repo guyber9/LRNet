f4d2244b0e8955c5c1f73477603f6e363dab056f
guyber9 <guyberger9@gmail.com>
2021-06-21 19:39:57 +0300

    optimize for GPU

---
+++ my_vgg.py
@@ -0,0 +1,68 @@
+'''VGG7 in Pytorch.'''
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class my_vgg(nn.Module):
+
+    def __init__(self):
+        super(my_vgg, self).__init__()
+        self.conv1 = nn.Conv2d(3, 128, 3, 1, padding=1)
+        self.conv2 = nn.Conv2d(128, 128, 3, 1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, 3, 1, padding=1)
+        self.conv4 = nn.Conv2d(256, 256, 3, 1, padding=1)
+        self.conv5 = nn.Conv2d(256, 512, 3, 1, padding=1)
+        self.conv6 = nn.Conv2d(512, 512, 3, 1, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.bn6 = nn.BatchNorm2d(512)
+        self.dropout1 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(8192, 1024)
+        self.fc2 = nn.Linear(1024, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)  # input is 3 x 32 x 32, output is 128 x 32 x 3
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.conv2(x) # 128 x 32 x 32
+        x = self.bn2(x)
+        x = F.max_pool2d(x, 2) # 128 x 16 x 16
+        x = F.relu(x)
+
+        x = self.conv3(x)  # 256 x 16 x 16
+        x = self.bn3(x)
+        x = F.relu(x)
+        x = self.conv4(x) # 256 x 16 x 16
+        x = self.bn4(x)
+        x = F.max_pool2d(x, 2) # 256 x 8 x 8
+        x = F.relu(x)
+
+        x = self.conv5(x)  # 512 x 8 x 8
+        x = self.bn5(x)
+        x = F.relu(x)
+        x = self.conv6(x) # 512 x 8 x 8
+        x = self.bn6(x)
+        x = F.max_pool2d(x, 2) # 512 x 4 x 4 (= 8192)
+        x = F.relu(x)
+
+        x = torch.flatten(x, 1) # 8192
+        x = self.dropout1(x)
+        x = self.fc1(x)  # 8192 -> 1024
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x) # 1024 -> 10
+        output = x
+        return output
+
+
+def test():
+    net = my_vgg()
+    x = torch.randn(2,3,32,32)
+    y = net(x)
+    print(y.size())
+
+# test()
